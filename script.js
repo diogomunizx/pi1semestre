eb651fd312a187d2a946215ee5ea7088e8f0b13b
Julia Barbosa <167313658+JuliaSBarbosa@users.noreply.github.com>
2024-11-12 15:37:59 -0300

    v 1.4
    ajustes js

---
+++ script.js
@@ -0,0 +1,36 @@
+function prepararRelatorio() {
+    // Seleciona a tabela e o formulário de relatório pelo ID correto
+    const tabelaProfessor = document.getElementById("tableProfessor");
+    const tabelaCoordenador= document.getElementById("tableCoordenador");
+    const tituloCoordenador = document.getElementById("tituloCoordenador");
+    const formularioProfessor = document.getElementById("formularioProfessor");
+    
+    // Verifica se ambos os elementos existem antes de tentar exibir ou ocultar
+    if (tabelaProfessor && tabelaCoordenador && formularioProfessor && tituloCoordenador) {
+        tabelaProfessor.style.display = "none";   // Esconde 
+        tabelaCoordenador.style.display = "none";   // Esconde 
+        tituloCoordenador.style.display = "none"; // Esconde 
+        formularioProfessor.style.display = "block";   // Exibe o formulário
+    } else {
+        console.error("Erro: Elemento de tabela ou formulário não encontrado.");
+    }
+}
+
+function deferirRelatorio() {
+    // Seleciona a tabela e o formulário de relatório pelo ID correto
+    const tabelaCoordenador = document.getElementById("tableCoordenador");
+    const formularioCoordenador = document.getElementById("formularioCoordenador");
+    const tabelaProfessor = document.getElementById("tableProfessor");
+    const tituloProfessor = document.getElementById("tituloProfessor");
+    
+    // Verifica se ambos os elementos existem antes de tentar exibir ou ocultar
+    if (tabelaCoordenador && formularioCoordenador && tabelaProfessor && tituloProfessor) {
+        tabelaCoordenador.style.display = "none";   // Esconde a tabela de inscrições
+        formularioCoordenador.style.display = "block";   // Exibe o formulário de edição
+        tabelaProfessor.style.display = "none";   // Esconde 
+        tituloProfessor.style.display = "none"; // Esconde 
+    } else {
+        console.error("Erro: Elemento de tabela ou formulário não encontrado.");
+    }
+}
+
